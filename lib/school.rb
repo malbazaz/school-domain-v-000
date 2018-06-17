f354755664ad467d4733d0add03b0bc20bc60660
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-17 04:34:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+attr_accessor :school, :add_student 
+
+
+
+def initialize(school_name)
+  @school = school_name 
+end 
+
+def roster
+@roster = {}
+end 
+
+
+def add_student(name, grade)
+@school.roster = {}
+school[grade]=name 
+
+end 
+
+end 
