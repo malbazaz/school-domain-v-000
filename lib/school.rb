e49571e76b9eb6d6823fda05090b9b2347cae5ce
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-17 04:41:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,18 +6,18 @@ attr_accessor :school, :add_student
 
 
 def initialize(school_name)
-  @school = school_name 
+  @school1 = school_name 
 end 
 
 def roster
-@school = {}
+@school1 = {}
 end 
 
 
 def add_student(name, grade)
 
-@school[grade] = []
-@school[grade] << name 
+@school1[grade] = []
+@school1[grade] << name 
 
 end 
 
